685d91f53b5795d179a5b056e854cb0ea10ad864
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2025-01-26 08:21:09 +0600

    Main

---
+++ khabarAche-app/src/components/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homepage.css'
+const Home = () => {
+    return (
+        <div classname="Home">
+        <div classname="ProfileSide">Profile</div>
+        <div classname="PostSide">Posts</div>
+        <div classname="RightSide">RightSide</div>
+        </div>
+    )
+}
+export default Home
